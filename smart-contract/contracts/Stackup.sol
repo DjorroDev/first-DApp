40e588be248b76493b1e7210e729b2072ffe5aa7
Djorro <DjorroDev@users.noreply.github.com>
2023-06-01 22:59:00 +0700

    Add Review features and improves Quest enum, struct and submission

--- smart-contract/contracts/Stackup.sol
+++ smart-contract/contracts/Stackup.sol
@@ -2,10 +2,13 @@
 pragma solidity ^0.8.4;
 
 contract StackUp {
-    enum playerQuestStatus {
+    enum PlayerQuestStatus {
         NOT_JOINED,
         JOINED,
-        SUBMITTED
+        SUBMITTED,
+        REJECTED,
+        APPROVED,
+        REWARDED
     }
     struct Quest {
         uint256 questId;
@@ -18,8 +21,16 @@ contract StackUp {
     address public admin;
     uint256 public nextQuestId;
     mapping(uint256 => Quest) public quests;
-    mapping(address => mapping(uint256 => playerQuestStatus))
+    mapping(address => mapping(uint256 => PlayerQuestStatus))
         public playerQuestStatuses;
+    mapping(address => mapping(uint256 => string)) public playerSubmissions; //Player submission a string type
+
+    // An event listening and storing log of player submission
+    event QuestSubmission(
+        uint256 indexed questId,
+        address indexed player,
+        string submission
+    );
 
     constructor() {
         admin = msg.sender;
@@ -29,38 +40,69 @@ contract StackUp {
         string calldata title_,
         uint8 reward_,
         uint256 numberOfRewards_
-    ) external {
-        require(msg.sender == admin, "Only the admin can create quests");
+    ) external onlyAdmin {
         quests[nextQuestId].questId = nextQuestId;
         quests[nextQuestId].title = title_;
         quests[nextQuestId].reward = reward_;
         quests[nextQuestId].numberOfRewards = numberOfRewards_;
+
         nextQuestId++;
     }
 
     function joinQuest(uint256 questId) external questExists(questId) {
         require(
             playerQuestStatuses[msg.sender][questId] ==
-                playerQuestStatus.NOT_JOINED,
+                PlayerQuestStatus.NOT_JOINED,
             "Player has already joined/submitted this quest"
         );
-        playerQuestStatuses[msg.sender][questId] = playerQuestStatus.JOINED;
+        playerQuestStatuses[msg.sender][questId] = PlayerQuestStatus.JOINED;
 
         Quest storage thisQuest = quests[questId];
         thisQuest.numberOfPlayers++;
     }
 
-    function submitQuest(uint256 questId) external questExists(questId) {
+    function submitQuest(
+        uint256 questId,
+        string calldata submission
+    ) external questExists(questId) {
         require(
             playerQuestStatuses[msg.sender][questId] ==
-                playerQuestStatus.JOINED,
+                PlayerQuestStatus.JOINED,
             "Player must first join the quest"
         );
-        playerQuestStatuses[msg.sender][questId] = playerQuestStatus.SUBMITTED;
+
+        playerQuestStatuses[msg.sender][questId] = PlayerQuestStatus.SUBMITTED;
+        playerSubmissions[msg.sender][questId] = submission;
+
+        // Emitting data Quest submission value to the event
+        emit QuestSubmission(questId, msg.sender, submission);
+    }
+
+    function reviewQuest(
+        uint256 questId,
+        uint256 status,
+        address playerAddr
+    ) external onlyAdmin questExists(questId) {
+        require(
+            playerQuestStatuses[playerAddr][questId] ==
+                PlayerQuestStatus.SUBMITTED,
+            "The player need to submitt the quest"
+        );
+        require(status >= 3 && status <= 5, "Invalid status"); // Validate the status value
+        // The uint status value based on the enum order of PlayerQuestStatus
+        // Start from 3 to 5 Which is REJECTED, APPROVED, REWARDED
+
+        playerQuestStatuses[playerAddr][questId] = PlayerQuestStatus(status);
     }
 
     modifier questExists(uint256 questId) {
         require(quests[questId].reward != 0, "Quest does not exist");
+        _;
+    }
+
+    // Adding modifier for admin only to make code not redundant
+    modifier onlyAdmin() {
+        require(msg.sender == admin, "Only the admin can perform this action");
         _;
     }
 }
